cce8bdc96b13e046487aa1f8d1594c90c1a39cf7
huy <okey222k@gmail.com>
2022-02-18 19:13:01 +0700

    fetch data

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+import axios from "axios";
 
-
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // fetchData
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      // fetch
+      // const res = await fetch(url);
+      // const tours = await res.json();
+      
+      // axios
+      const res = await axios.get(url);
+      const data = res.data; 
+      setTours(data)
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours/>
+    </main>
+  );
 }
 
 export default App;
