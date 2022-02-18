00b3d4a3efaa72d3f6256789e0410d3d51c87143
huy <okey222k@gmail.com>
2022-02-18 17:20:57 +0700

    done

--- 01-birthday/src/App.js
+++ 01-birthday/src/App.js
@@ -1,9 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import List from './List';
+import data from './data';
 
 function App() {
+  const [people,setPeople] = useState(data)
   return (
-    <h2>setup</h2>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}/>
+        <button onClick={()=>setPeople([])}>
+          clear all
+        </button>
+      </section>
+    </main>
     );
 }
 
--- 01-birthday/src/List.jsx
+++ 01-birthday/src/List.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
-const List = () => {
+const List = ({people}) => {
   return (
-    <div>List</div>
+      <>
+        {people && people.length > 0 &&
+            people.map(person =>(
+                <article key={person.id} className="person">
+                    <img src={person.image} alt={person.name}/>
+                    <div>
+                        <h4>{person.name}</h4>
+                        <h4>{person.age} years</h4>
+                    </div>
+                </article>
+            ))
+        }
+      </>
   )
 }
 
